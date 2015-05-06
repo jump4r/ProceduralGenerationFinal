48bd059d021fd8471214e4c2cb6fea400c1ef778
Andrew Morris <jump3r@dggaming.net>
2015-05-06 04:16:36 -0400

    Added mesh and procedural skeleton code.

---
+++ Audio Based Procedural Mesh/Assets/Scripts/APITest.cs
@@ -0,0 +1,23 @@
+﻿    using UnityEngine;
+using System.Collections;
+using BMAPI;
+using BMAPI.v1;
+public class APITest : MonoBehaviour
+{
+
+    BMAPI.v1.Beatmap beatmap = new BMAPI.v1.Beatmap("D:\\osu\\Songs\\207806 Tujamo & Plastik Funk feat Sneakbo - Dr Who! (Smooth Remix)\\Tujamo & Plastik Funk feat. Sneakbo - Dr. Who! (Smooth Remix) (Natteke) [Extra].osu");
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log(beatmap.ApproachRate);
+        Debug.Log(beatmap.Artist);
+        Debug.Log(beatmap.OverallDifficulty);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Audio Based Procedural Mesh/Assets/Scripts/OpenFileDialog.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using BMAPI.v1;
+
+public class OpenFileDialog : MonoBehaviour {
+
+    private Beatmap beatmap;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void SelectFile()
+    {
+        string path = EditorUtility.OpenFilePanel("Select .osu Beatmap File", "./Beatmaps", "osu");
+        if (path.Length != 0)
+        {
+            // var www = WWW("file:///" + path);
+            Debug.Log("Beatmap Loaded");
+            beatmap = new Beatmap(path);
+        }
+    }
+}
